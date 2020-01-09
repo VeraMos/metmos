496ad42b11f9f26635a29f1d45df116440042617
Vera <verkaevg@.gmail.com>
2020-01-09 16:22:55 +0400

    add delivery

--- app/js/scripts.js
+++ app/js/scripts.js
@@ -160,12 +160,55 @@ $(document).ready(function () {
         });
 
 
-    // $("#mmenu").mmenu({
-    //       extensions : [ "position-bottom", "listview-50", "fx-listitems-drop", "border-offset" ],
-    //       autoHeight : false,
-    //       navbar: { title: "Меню" }
-    //     });
-    // });
+   $(document).ready(function() {
+
+    var $menu = $("#my-menu").mmenu();
+
+    var $icon = $("#mmenu-icon");
+
+    var API = $menu.data( "mmenu" );
+
+
+    $icon.on( "click", function() {
+
+        API.open();
+
+    });
+
+
+    API.bind( "opened", function() {
+
+       setTimeout(function() {
+
+          $icon.addClass( "is-active" );
+
+       }, 100);
+
+       $icon.on( "click", function() {
+
+          API.close();
+
+       });
+
+   });
+
+   API.bind( "closed", function() {
+
+      setTimeout(function() {
+
+         $icon.removeClass( "is-active" );
+
+      }, 100);
+
+      $icon.on( "click", function() {
+
+         API.open();
+
+      });
+
+   });
+
+});
 
     
      $('input, select').styler({
