1317e31f2e84825611e95592475f4eeacd9d3c26
Vera <verkaevg@.gmail.com>
2020-01-10 09:23:43 +0400

    add pravki

--- app/js/scripts.js
+++ app/js/scripts.js
@@ -160,55 +160,12 @@ $(document).ready(function () {
         });
 
 
-   $(document).ready(function() {
-
-    var $menu = $("#my-menu").mmenu();
-
-    var $icon = $("#mmenu-icon");
-
-    var API = $menu.data( "mmenu" );
-
-
-    $icon.on( "click", function() {
-
-        API.open();
-
-    });
-
-
-    API.bind( "opened", function() {
-
-       setTimeout(function() {
-
-          $icon.addClass( "is-active" );
-
-       }, 100);
-
-       $icon.on( "click", function() {
-
-          API.close();
-
-       });
-
-   });
-
-   API.bind( "closed", function() {
-
-      setTimeout(function() {
-
-         $icon.removeClass( "is-active" );
-
-      }, 100);
-
-      $icon.on( "click", function() {
-
-         API.open();
-
-      });
-
-   });
-
-});
+    // $("#mmenu").mmenu({
+    //       extensions : [ "position-bottom", "listview-50", "fx-listitems-drop", "border-offset" ],
+    //       autoHeight : false,
+    //       navbar: { title: "Меню" }
+    //     });
+    // });
 
     
      $('input, select').styler({
